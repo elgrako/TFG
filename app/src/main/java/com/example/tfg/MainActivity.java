5a6e96ac054e5a47ef73e28ac03e862ee3328232
elgrako <elgraco363@gmail.com>
2025-03-03 17:26:34 +0100

    1.0 Inicio

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tfg;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ListView listaDatos = findViewById(R.id.listaDatos);
     }
 }
